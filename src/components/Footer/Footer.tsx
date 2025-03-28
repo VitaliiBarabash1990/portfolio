6b78b8111b00ebfaddbb6dd269cc602bc625fef4
vitalii-pc <alievruslan1818@gmail.com>
2025-03-28 12:33:53 +0100

    delete footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,23 +4,5 @@ import s from './Footer.module.css';
 
 export default function Footer() {
   const t = useTranslations('footer');
-  return (
-    <div className={`container ${s.footer__container}`}>
-      <ul className={s.footer__left_list}>
-        <li>
-          <Link href={{pathname: '/', hash: 'Services'}} rel="stylesheet">
-            {t('our_servises')}
-          </Link>
-        </li>
-      </ul>
-
-      <ul className={s.footer__center_list}>
-        <li>
-          <Link href={{pathname: '/', hash: 'About'}} rel="stylesheet">
-            {t('about_master')}
-          </Link>
-        </li>
-      </ul>
-    </div>
-  );
+  return <></>;
 }
