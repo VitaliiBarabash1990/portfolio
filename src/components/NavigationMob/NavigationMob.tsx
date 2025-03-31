3b731873d8c0cc454be273bfed50d3b7a2ee5c62
vitalii-pc <alievruslan1818@gmail.com>
2025-03-31 23:12:02 +0200

    clear navigationMob

--- src/components/NavigationMob/NavigationMob.tsx
+++ src/components/NavigationMob/NavigationMob.tsx
@@ -19,23 +19,5 @@ export const NavigationMob = ({isModal, setMenuOpen}: MyComponentProps) => {
     isModal && setMenuOpen(false);
   };
 
-  // const t = useTranslations('Navigation');
-
-  // const linkDatas: LinkData[] = [{id: 0, link: '/', text: t('')}];
-
-  return (
-    <ul className={clsx(s.navMenu, isModal && s.navMenu_modal)}>
-      {linkDatas.map((linkData) => (
-        <li key={linkData.id} className={s.navMenu__item_link}>
-          <NavigationLink
-            className={s.navMenu__link}
-            href={linkData.link}
-            onClick={handlerSubmit}
-          >
-            {linkData.text}
-          </NavigationLink>
-        </li>
-      ))}
-    </ul>
-  );
+  return <ul className={clsx(s.navMenu, isModal && s.navMenu_modal)}></ul>;
 };
